71fa6b23973ea7cda3f4a3039c7977eca3b3c721
Gianmarco G <giangarof98@gmail.com>
2022-02-28 21:50:00 -0500

    colorBox style

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ class App extends Component{
   render(){
     return (
       <div>
-        <Palette />
+        <Palette {...seedColors[1]} />
       </div>
     )
   }
 }
 
-export default App
+export default App;
